cce0f8bbd8fccfb7ce094ee99ca166d5dc87192f
Angela Mitchell <anglarenmit@gmail.com>
2018-10-07 17:46:13 -0500

    Works in GitBash, but not in main py or blog html

--- main.py
+++ main.py
@@ -23,20 +23,19 @@ def blog():
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
+
     if request.method == 'POST':
         blog_title = request.form['title']
         blog_body = request.form['body']
         new_post = Blog(blog_title, blog_body)
-        db.session.add(new_post)
-        db.session.commit()
-
-    posts = Blog.query.filter_by(blog_title=blog_title).all()
-
-    if len(blog_title) != 0 and len(blog_body) != 0:
-        flash('New post created')
-        return redirect('/blog', posts=posts)
-    else:
-        flash('Blog title or blog content invalid')
+        
+        if len(blog_title) != 0 and len(blog_body) != 0:
+            db.session.add(new_post)
+            db.session.commit()
+            flash('New post created')
+            return redirect('/blog', new_post=new_post)
+        else:
+            flash('Blog title or blog content invalid')
 
     return render_template('newpost.html')
 
